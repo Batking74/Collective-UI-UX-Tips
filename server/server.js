227b23aeb8616aa9875fd6d1f541467613fc3cc2
Nazir Knuckles <nazirknuckles@gmail.com>
2024-03-19 07:42:25 -0400

    Altered json file

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { connection } = require('./database/database');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
