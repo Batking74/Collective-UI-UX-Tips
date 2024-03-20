9dc503088cf8eb1715e906af89191c4589945758
Nazir Knuckles <nazirknuckles@gmail.com>
2024-03-20 01:05:41 -0400

    Made changes to App in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,14 @@ import { Outlet } from 'react-router-dom';
 import React from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+let uri = 'http://localhost:5000/graphQL';
+if(location.hostname != 'localhost') {
+  uri = 'https://openchat-0ptg.onrender.com/graphQL';
+}
+
 // Apollo Client handles sending GraphQL queries and mutations to the graphQL server and managing the data returned from those operations.
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphQL',
+  uri,
   // When ApolloClient sends a query to the server and receives a response, it stores the response data in the cache.
   cache: new InMemoryCache()
 });
