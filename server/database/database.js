e23f5ee6c0cfa944bd224542ef9341ab63498eed
Nazir Knuckles <nazirknuckles@gmail.com>
2024-03-20 20:57:25 -0400

    Changed the database connection string to my machine address

--- server/database/database.js
+++ server/database/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Connecting to Database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/OpenChat');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/OpenChat');
 
 // Exporting Module
 module.exports = mongoose;
