fa2ce9615643a0bccbafac39e75a08c12167fc0a
Nazir Knuckles <nazirknuckles@gmail.com>
2024-03-20 21:40:23 -0400

    Changed middleware in server.js file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Importing Modules/Packages
 const { connection } = require('./database/database');
-const { graphqlHTTP } = require('express-graphql');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const schema = require('./schema/schema');
 const express = require('express');
 const cors = require('cors');
@@ -8,27 +9,40 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = { origin: ['http://localhost:5173', 'https://openchat-0ptg.onrender.com'] };
 
-
-// Creating a route for GraphQL API
-app.use('/graphQL', cors(corsOptions), graphqlHTTP({
-    schema,
-    graphiql: true
-}));
+const server = new ApolloServer({ schema });
 
 
-// Starting Server
-app.listen(PORT, () => {
-    console.log(`Listening on Port ${PORT}!`);
-    // Listening for Database open
-    connection.once('open', () => {
-        console.log('Database connected successfully!');
+const startApolloServer = async () => {
+
+    // Starting Apollo Server
+    await server.start();
+
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+
+    // Creating API Route for Apollo Server
+    app.use('/graphQL', cors(corsOptions), expressMiddleware(server));
+
+    // Starting Server
+    app.listen(PORT, () => {
+        console.log(`Listening on Port ${PORT}!`);
+        
+        // Listening for Database open
+        connection.once('open', () => {
+            console.log('Database connected successfully!');
+        })
+        .on('error', (error) => {
+            console.error(`Database Connection Error: ${error}`);
+            throw error;
+        })
     })
     .on('error', (error) => {
-        console.error(`Database Connection Error: ${error}`);
+        console.error(`Server Error: ${error}`);
         throw error;
     })
-})
-.on('error', (error) => {
-    console.error(`Server Error: ${error}`);
-    throw error;
-})
+}
+
+
+startApolloServer();
+
+
